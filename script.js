07a820f97a74fd2065975a9d36fc37677aa78778
KontentManufaktura <kontentmanufaktura@gmail.com>
2023-10-24 16:04:43 +0200

    new text + contact form email

--- script.js
+++ script.js
@@ -106,10 +106,10 @@ document.addEventListener('DOMContentLoaded', function () {
 
 var languages = {
     en: {
-        buttonText: "HUN",
-        navbarServices: "Services",
-        navbarAbout: "About us",
-        navbarContact: "Contact",
+        buttonText: "hun",
+        navbarServices: "services",
+        navbarAbout: "about us",
+        navbarContact: "contact",
         contactSalesText: "If you also want to reach your target audience with content that triggers interaction, let's start working together. Write to us in the form below how we can help your business, and we will get back to you within 3 working days.",
         contactGetInContactText: "Contact us:",
         contactFormName: "Name",
@@ -117,24 +117,25 @@ var languages = {
         contactFromMessage: "Message",
         contactFormInterests: "Interests",
         contactFormSocial: "Social media management",
-        contactFormVideo: "Video",
-        contactFormPhoto: "Photo",
+        contactFormVideo: "Photo",
+        contactFormPhoto: "Video",
         contactPageTel: "Phone",
 
         //homepage
-        greetingTextHomePage: "Hello, we are KOMA. Three creative freelancers with numerous years of creative agency experience behind us have formed a team to deliver your brand's messages on social media with truly creative multimedia content and advertising campaigns.",
-        homePageTeamText: "Team",
-        homePageIngridText: "Project coordinator",
-        homePageGergoText: "Graphic designer",
-        homePageServicesText: "Services",
+        greetingTextHomePage: "Hello, we are KOMA. Three creative freelancers with numerous years of creative agency experience behind us have formed a team to deliver your brand's messages on social media with truly creative multimedia content and advertising campaigns. Our mission is to demonstrate that with honesty and authenticity, a new direction can be achieved for everyone in the world of marketing.",
+        homePageTeamText: "team",
+        homePageEdmondText: "Social media manager",
+        homePageIngridText: "Project manager",
+        homePageGergoText: "Grapich designer, video producer",
+        homePageServicesText: "services",
         homePageSocialServiceText: "Social media management",
         homePagePhotoServiceText: "Photography",
         homePageVideoServiceText: "Film and video production",
         submitButtonText: "Send",
 
         //services
-        servicesSubTitle: "Services",
-        servicesGreeting: "Social media is part of our daily lives, but for us it's also our job and hobby. With our multimedia content production, our goal is to make our partners visible and their messages reach the right audience.Get to know our previous works by clicking on the pictures and let's start a new project together! ",
+        servicesSubTitle: "services",
+        servicesGreeting: "Social media is part of our daily lives, but for us it's also our job and hobby. With our multimedia content production, our goal is to make our partners visible and their messages reach the right audience. Get to know our previous works by clicking on the pictures and let's start a new project together!",
         servicesSocialtitle: "Social media management",
         servicesSocialText: "Management of social media platforms: creating a communication strategy, writing and producing posts, designing graphic and branding elements, coordinating online campaigns, monitoring results, and managing comments/messages.",
         servicesVideoTitle: "Film and video production",
@@ -144,20 +145,21 @@ var languages = {
 
 
         //team
-        teamsubTitle: "About us",
-        teamTextAboutUs: "Thanks to so many coincidences, our team came together, and we started our joint work in 2020.When we founded KOMA, we pooled our knowledge, set common goals, and started to implement our plans.The field of social media has been part of our daily work for many years, and we supplement the task of video production as well.We have participated in various projects in the past, and we apply our agency experience to the best possible advantage.Fortunately, our interests are diverse, and every day, we complement each other with our knowledge to get the most out of our assignments.With our projects, the goal is to provide creative content to our customers, as well as create value and gain as much interaction as possible from the target audience.",
+        teamsubTitle: "about us",
+        teamTextAboutUs: "Thanks to so many coincidences, our team came together, and we started our joint work in 2020. When we founded KOMA, we pooled our knowledge, set common goals, and started to implement our plans. The field of social media has been part of our daily work for many years, and we supplement the task of video production as well. We have participated in various projects in the past, and we apply our agency experience to the best possible advantage. Fortunately, our interests are diverse, and every day, we complement each other with our knowledge to get the most out of our assignments. With our projects, the goal is to provide creative content to our customers, as well as create value and gain as much interaction as possible from the target audience.",
+        edmondTeamRole: "Social media manager",
         edmondTeamText: "As a social media manager, my goal is to produce fun, but also result-oriented content. I have spent several years working at creative agencies and gained valuable experience in campaign planning, copywriting, advertising, and community management. I studied as a director and screenwriter at the Werk Academy's film school. I primarily managed the online presence of hospitality establishments, such as hotels and restaurants, but I also directed short films and wrote scripts for branding videos.",
-        ingridTeamRole: "Project coordinator",
-        ingridTextTeam: "As a project coordinator and copywriter, it's important to me to create value with our team. Not only to reach users with our content but to achieve interaction through it. I have worked with local small businesses and global brands, ranging from household appliances to wristwatches, so it's safe to say I've worked on almost everything. However, my heart is closest to culture and hospitality, and I'm also willing to stand up for social causes; I volunteered for several years, for example.",
-        gergoTeamRole: "Graphic designer",
-        gergoTextTeam: "I am responsible for the visual aspect of posts to ensure they align with the brand identity and handle the practical aspects of video projects from pre- to post-production. I studied at Werk Academy's film school, now finishing at MOME Media Design. My experiences include designing and executing installations, creating music videos, producing promotional campaigns for the Hungarian Orienteering Federation, live visual performances at techno events, directing shorts/image videos, and so on."
+        ingridTeamRole: "Project manager",
+        ingridTextTeam: "As a project manager and copywriter, it's important to me to create value with our team. Not only to reach users with our content but to achieve interaction through it. I have worked with local small businesses and global brands, ranging from household appliances to wristwatches, so it's safe to say I've worked on almost everything. However, my heart is closest to culture and hospitality, and I'm also willing to stand up for social causes; I volunteered for several years, for example.",
+        gergoTeamRole: "Grapich designer, video producer",
+        gergoTextTeam: "I am responsible for the visual aspect of posts to ensure they align with the brand identity and carry the practical aspects of video projects on my shoulders, from planning to post-production. I studied at Werk Academy's film school, now finishing at MOME Media Design. My experiences include designing and executing installations, creating music videos, producing promotional campaigns for the Hungarian Orienteering Federation, live visual performances at techno events, directing shorts/image videos, and so on."
 
     },
     hu: {
-        buttonText: "EN",
-        navbarServices: "Szolgáltatások",
-        navbarAbout: "Rólunk",
-        navbarContact: "Kapcsolat",
+        buttonText: "en",
+        navbarServices: "szolgáltatások",
+        navbarAbout: "rólunk",
+        navbarContact: "kapcsolat",
         contactSalesText: "Ha te is szeretnéd elérni a célközönséged olyan tartalmakkal, amelyek interakciót váltanak ki, akkor kezdjük el a közös munkát. Írd meg nekünk az alábbi űrlapon, hogy miben tudunk segíteni a vállalkozásodnak és mi 3 munkanapon belül visszajelzünk.",
         contactGetInContactText: "Lépj velünk kapcsolatba!",
         contactFormName: "Név",
@@ -171,18 +173,19 @@ var languages = {
         submitButtonText: "Küldés",
 
         //homepage
-        greetingTextHomePage: "Hello, mi vagyunk a KOMA! Három kreatív szabadúszó, többéves reklámügynökségi tapasztalattal a hátunk mögött összeálltuk egy csapatba, hogy igazán kreatív multimédiás tartalmakkal és hirdetési kampányokkal célba juttassuk a márkád üzeneteit a közösségi médiában.",
-        homePageTeamText: "Csapatunk",
-        homePageIngridText: "Projekt koordinátor",
-        homePageGergoText: "Grafikus",
-        homePageServicesText: "Szolgáltatások",
+        greetingTextHomePage: "Hello, mi vagyunk a KOMA! Három kreatív szabadúszó, többéves reklámügynökségi tapasztalattal a hátunk mögött összeálltuk egy csapatba, hogy igazán kreatív multimédiás tartalmakkal és hirdetési kampányokkal célba juttassuk a márkád üzeneteit a közösségi médiában. Küldetésünk megmutatni azt, hogy őszinteséggel, hitelességgel mindenki számára új irány érhető el a marketing világában.",
+        homePageTeamText: "csapatunk",
+        homePageEdmondText: "Közösségi média menedzser",
+        homePageIngridText: "Projekt menedzser",
+        homePageGergoText: "Grafikus, videógyártó",
+        homePageServicesText: "szolgáltatások",
         homePageSocialServiceText: "Közösségi média menedzsment",
         homePagePhotoServiceText: "Fotózás",
         homePageVideoServiceText: "Film- és videógyártás",
 
         //servicespage
-        servicesSubTitle: "Szolgáltatások",
-        servicesGreeting: "Mindennapjaink része a közösségi média, nekünk pedig a digitális marketing a munkánk és a hobbink is egyben. Multimédiás tartalomgyártásunkkal az a célunk, hogy partnereink láthatóvá váljanak és üzeneteik elérjék a megfelelő közönséget.Ismerd meg korábbi munkáinkat a képekre kattintva és kezdjünk bele együtt egy új projektbe! ",
+        servicesSubTitle: "szolgáltatások",
+        servicesGreeting: "Mindennapjaink része a közösségi média, nekünk pedig a digitális marketing a munkánk és a hobbink is egyben. Multimédiás tartalomgyártásunkkal az a célunk, hogy partnereink láthatóvá váljanak és üzeneteik elérjék a megfelelő közönséget. Ismerd meg korábbi munkáinkat a képekre kattintva és kezdjünk bele együtt egy új projektbe! ",
         servicesSocialtitle: "Közösségi média menedzsment",
         servicesSocialText: "Közösségi felületek menedzselése: kommunikációs stratégia megalkotása, szövegírás és posztok gyártása, grafikai és arculati elemek megalkotása, online kampányok koordinálása, eredmények nyomon követése, valamint kommentek/üzenetek kezelése.",
         servicesVideoTitle: "Filmek és videók",
@@ -192,13 +195,14 @@ var languages = {
 
 
         //team
-        teamsubTitle: "Rólunk",
-        teamTextAboutUs: "Csapatunk számos véletlennek köszönhetően sodródott össze, a közös munkánkat pedig 2020-ban kezdtük el. A KOMA megalapításakor összegyúrtuk tudásunkat, közös célokat állítottunk magunk elé és nekivágtunk megvalósítani terveinket. A social media területe sok éve mindennapi munkánk része, ezt később kiegészítettük a videókészítés feladatával. Korábban változatos projektekben vettünk részt, ügynökségi tapasztalatunkat pedig próbáljuk a lehető legelőnyösebben felhasználni. Szerencsére sokrétű az érdeklődési körünk, tudásunkkal igyekszünk kiegészíteni egymást és a maximálisat kihozni a megbízásainkból.Projektjeink által a célunk, hogy ne csupán kreatív tartalmakkal lássuk el ügyfeleinket, hanem értéket is teremtsünk és minél több interakciót váltsunk ki az adott célközönségből.",
+        teamsubTitle: "rólunk",
+        teamTextAboutUs: "Csapatunk számos véletlennek köszönhetően sodródott össze, a közös munkánkat pedig 2020-ban kezdtük el. A KOMA megalapításakor összegyúrtuk tudásunkat, közös célokat állítottunk magunk elé és nekivágtunk megvalósítani terveinket. A social media területe sok éve mindennapi munkánk része, ezt később kiegészítettük a videókészítés feladatával. Korábban változatos projektekben vettünk részt, ügynökségi tapasztalatunkat pedig próbáljuk a lehető legelőnyösebben felhasználni. Szerencsére sokrétű az érdeklődési körünk, tudásunkkal igyekszünk kiegészíteni egymást és a maximálisat kihozni a megbízásainkból. Projektjeink által a célunk, hogy ne csupán kreatív tartalmakkal lássuk el ügyfeleinket, hanem értéket is teremtsünk és minél több interakciót váltsunk ki az adott célközönségből.",
+        edmondTeamRole: "Közösségi média menedzser",
         edmondTeamText: "Közösségi média menedzserként célom, hogy szórakoztató, mégis eredményorientált tartalmakat állítsak elő. Több évet töltöttem el kreatív ügynökségeknél, ahol értékes tapasztalatokat szereztem kampánytervezésben, szövegírásban, hirdetéskezelésben és community managementben. A Werk Akadémia filmes szakján tanultam rendezőként és forgatókönyvíróként. Leginkább vendéglátóegységek, például hotelek és éttermek oldalait menedzseltem, de rendeztem rövidfilmeket és forgatókönyveket írtam imázsfilmekhez.",
-        ingridTeamRole: "Projekt koordinátor",
-        ingridTextTeam: "Projekt koordinátorként és szövegíróként fontos számomra, hogy csapatunkkal értéket hozzunk létre, amelyekkel nemcsak eljussunk tartalmainkkal a felhasználókhoz, hanem interakciót érjünk el általa. Dolgoztam hazai kisvállalkozásokkal és világmárkákkal egyaránt, háztartási gépektől kezdve karórákig, tehát szinte tényleg mindenen. A szívemhez mégis legközelebb a kultúra és a vendéglátás áll, de társadalmi ügyek mellett is szívesen kiállok, több évig önkénteskedtem például.",
-        gergoTeamRole: "Grafikus",
-        gergoTextTeam: "Én felelek a posztok kinézetéért, hogy minden klappoljon a megrendelő arculatával és én viszem a videós projektek gyakorlati részét a forgatás megtervezésétől az utómunkáig. Korábban a Werk Akadémia filmes szakára jártam, most a MOME Média Design utolsó éves hallgatója vagyok. Tapasztalataim igen sokszínűek: installációk tervezése és kivitelezése, videóklip-készítés, a Magyar Tájékozódási Futó Szövetség videós népszerűsítő kampányai, live vizuál techno bulin, rövid- és imázsfilmrendezés stb. "
+        ingridTeamRole: "Projekt menedzser",
+        ingridTextTeam: "Projekt menedzserként és szövegíróként fontos számomra, hogy csapatunkkal értéket hozzunk létre, amelyekkel nemcsak eljussunk tartalmainkkal a felhasználókhoz, hanem interakciót érjünk el általa. Dolgoztam hazai kisvállalkozásokkal és világmárkákkal egyaránt, háztartási gépektől kezdve karórákig, tehát szinte tényleg mindenen. A szívemhez mégis legközelebb a kultúra és a vendéglátás áll, de társadalmi ügyek mellett is szívesen kiállok, több évig önkénteskedtem például.",
+        gergoTeamRole: "Grafikus, videógyártó",
+        gergoTextTeam: "Én felelek a posztok kinézetéért, figyelek arra, hogy minden klappoljon a megrendelő arculatával, és nem utolsósorban, én viszem a hátamon a videós projektek gyakorlati részét, a tervezéstől az utómunkáig. Korábban a Werk Akadémia filmes szakára jártam, most a MOME Média Design utolsó éves hallgatója vagyok. Tapasztalataim igen sokszínűek: installációk tervezése és kivitelezése, videóklip-készítés, a Magyar Tájékozódási Futó Szövetség videós népszerűsítő kampányai, live vizuál techno bulin, rövid- és imázsfilmrendezés stb. "
     }
 };
 
@@ -227,9 +231,9 @@ function getCurrentPage() {
     // Check if the content includes both "Rólunk" and "About us" text
     if (contentText.includes("Hello, mi vagyunk a KOMA!") || contentText.includes("Hello, we are KOMA.")) {
         return "home.html";
-    } else if (contentText.includes("Rólunk") || contentText.includes("About us")) {
+    } else if (contentText.includes("rólunk") || contentText.includes("about us")) {
         return "team.html";
-    } else if (contentText.includes("Services") || contentText.includes("Szolgáltatások")) {
+    } else if (contentText.includes("services") || contentText.includes("szolgáltatások")) {
         return "services.html";
     } else {
         // Default to the home page if none of the page contents match
@@ -240,6 +244,7 @@ function getCurrentPage() {
 var elementsToTranslate = {
     "i18nbutton": "buttonText",
     "loadServices": "navbarServices",
+    "edmondRole": "homePageEdmondText",
     "loadTeam": "navbarAbout",
     "contactButon": "navbarContact",
     "salesText": "contactSalesText",
@@ -280,6 +285,7 @@ var elementsToTranslateServicesPage = {
 var elementsToTranslateTeamPage = {
     "teamsubTitle": "teamsubTitle",
     "teamTextAboutUs":  "teamTextAboutUs",
+    "edmondTeamRole": "edmondTeamRole",
     "edmondTeamText":  "edmondTeamText",
     "ingridTeamRole": "ingridTeamRole",
     "ingridTextTeam":  "ingridTextTeam",
@@ -315,7 +321,4 @@ function updateContent(language) {
         }
     }
 }
-
-// Usage example for dynamically determining the current page
-updateContent(currentLanguage);
 
